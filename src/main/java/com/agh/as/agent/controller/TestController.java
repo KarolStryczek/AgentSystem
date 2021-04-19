08313ab5175be37355ae26e4f2d691d06f385546
Froziak <marcingala121@interia.pl>
2021-04-19 22:08:57 +0200

    Initial.

--- src/main/java/com/agh/as/agent/controller/TestController.java
+++ src/main/java/com/agh/as/agent/controller/TestController.java
@@ -1,8 +1,21 @@
 package com.agh.as.agent.controller;
 
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.Collections;
 
+@RestController
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class TestController {
+
+
+    @GetMapping("/test")
+    public ResponseEntity testMessage() {
+        return ResponseEntity.ok(Collections.singletonMap("status", "UP"));
+    }
+
 }
