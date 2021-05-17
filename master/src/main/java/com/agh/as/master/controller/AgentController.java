5270859121a6a7503c795645eb078d4cad110283
Froziak <marcingala121@interia.pl>
2021-05-18 00:22:00 +0200

    Running instances heartbeat.

--- master/src/main/java/com/agh/as/master/controller/AgentController.java
+++ master/src/main/java/com/agh/as/master/controller/AgentController.java
@@ -4,6 +4,7 @@ package com.agh.as.master.controller;
 import com.agh.as.master.dto.request.RegisterAgentForm;
 import com.agh.as.master.service.RunningAgentsService;
 import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
 import lombok.Value;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
@@ -15,8 +16,9 @@ import javax.validation.Valid;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/agent")
-@FieldDefaults(level = AccessLevel.PRIVATE)
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class AgentController {
 
     RunningAgentsService runningAgentsService;
