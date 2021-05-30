f7c510e9a75fa01794e5e6267cfa49ba2d3ab608
Froziak <marcingala121@interia.pl>
2021-05-30 16:51:10 +0200

    Add queue service.

---
+++ queue-service/src/main/java/com/agh/as/queueservice/QueueServiceApplication.java
@@ -0,0 +1,13 @@
+package com.agh.as.queueservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueueServiceApplication.class, args);
+    }
+
+}
